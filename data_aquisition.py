72b707e9da3a39cabee028681da6293061e26914
Stefan <73125240+ownrepo1s@users.noreply.github.com>
2023-04-15 18:04:52 +0300

    refactor for readability

--- data_aquisition.py
+++ data_aquisition.py
@@ -1,40 +1,42 @@
 """
-Data aquisition module that imports the data from the csv
+Data acquisition module that imports the data from the csv
 
 """
+from pathlib import Path
 
 import pandas as pd
-from pathlib import Path
+
+CURRENT_PATH_CWD = Path.cwd()
 
 
 class GetData:
     """
-    Class of method(s) for inserting data
-
+    Class of method(s) for getting data
     """
 
-
-    def csv_insert(self, file_name:str)->pd.DataFrame:
+    def get_data(self, file_name: str) -> pd.DataFrame:
         """
-        Method that inserts from the cvs , removes nulls and renames some column title.
+        Method that creates a dataframe from the cvs, further processes by removing nulls and renames column titles.
+        Assumes the file name is at the same path as this file.
         It needs the Pandas import to work and also the Path library.
+        Raises: if the input csv file is missing or corrupted
         """
+        file_path = CURRENT_PATH_CWD / file_name
 
-        p=Path.cwd()
-        p=p/file_name
+        if not file_path.exists():
+            raise Exception(f'The expected input file name does not exist at path: {file_path}')
 
-        df=pd.read_csv(p, index_col=0)
+        df = pd.read_csv(filepath_or_buffer=file_path, index_col=0)
 
-        glucose=df[['Timestamp (YYYY-MM-DDThh:mm:ss)','Glucose Value (mg/dL)']]
+        glucose = df[['Timestamp (YYYY-MM-DDThh:mm:ss)', 'Glucose Value (mg/dL)']]
 
-        glucose['Timestamp (YYYY-MM-DDThh:mm:ss)']=pd.to_datetime(glucose['Timestamp (YYYY-MM-DDThh:mm:ss)'])
-    
-        glucose['Glucose Value (mg/dL)']=pd.to_numeric(glucose['Glucose Value (mg/dL)'],errors='coerce')
-    
-        glucose.dropna(inplace=True)  
+        glucose['Timestamp (YYYY-MM-DDThh:mm:ss)'] = pd.to_datetime(glucose['Timestamp (YYYY-MM-DDThh:mm:ss)'])
 
-        glucose['Glucose Value (mg/dL)']=glucose['Glucose Value (mg/dL)'].astype(float)
-        glucose= glucose.rename(columns={'Timestamp (YYYY-MM-DDThh:mm:ss)':'Timestamp'})
+        glucose['Glucose Value (mg/dL)'] = pd.to_numeric(glucose['Glucose Value (mg/dL)'], errors='coerce')
+
+        glucose.dropna(inplace=True)
+
+        glucose['Glucose Value (mg/dL)'] = glucose['Glucose Value (mg/dL)'].astype(float)
+        glucose = glucose.rename(columns={'Timestamp (YYYY-MM-DDThh:mm:ss)': 'Timestamp'})
 
         return glucose
-    
