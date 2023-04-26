118ed1e71cd8db29a8f7acad22c227b2f94a0253
Stefan <73125240+ownrepo1s@users.noreply.github.com>
2023-04-27 01:34:40 +0300

    beautify code

--- data_acquisition.py
+++ data_acquisition.py
@@ -2,67 +2,142 @@
 Data acquisition module that imports the data from the csv
 
 """
+
 from pathlib import Path
+
 import pandas as pd
 
 CURRENT_PATH_CWD = Path.cwd()
 
 
-def get_data(file_name: str) -> pd.DataFrame:
+def read_csv_file(file_name: str) -> pd.DataFrame:
     """
-    Method that creates a dataframe with the glucose from the cvs, further processes by removing nulls and renames column titles.
-    Assumes the file name is at the same path as this file.
-    It needs the Pandas import to work and also the Path library.
-    Raises: if the input csv file is missing or corrupted
+    Read the input CSV file and return the DataFrame.
+
+    Args:
+        file_name: The name of the input CSV file.
+
+    Returns:
+        A Pandas DataFrame containing the CSV data.
+
+    Raises:
+        Exception: If the input CSV file is missing or corrupted.
     """
+    # Combine the current working directory path with the file name
     file_path = CURRENT_PATH_CWD / file_name
 
+    # Check if the file exists
     if not file_path.exists():
+        # Raise an exception if the file is missing
         raise Exception(f'The expected input file name does not exist at path: {file_path}')
 
+    # Read the CSV file and return the DataFrame
     df = pd.read_csv(filepath_or_buffer=file_path, index_col=0)
 
-    glucose = df[['Timestamp (YYYY-MM-DDThh:mm:ss)', 'Glucose Value (mg/dL)']]
+    return df
 
-    glucose['Timestamp (YYYY-MM-DDThh:mm:ss)'] = pd.to_datetime(glucose['Timestamp (YYYY-MM-DDThh:mm:ss)'])
 
-    glucose['Glucose Value (mg/dL)'] = pd.to_numeric(glucose['Glucose Value (mg/dL)'], errors='coerce')
+def get_insulin_data(file_name: str) -> pd.DataFrame:
+    """
+    Create a dataframe containing insulin data from a given CSV file, preprocess it by removing nulls and renaming columns.
 
-    glucose.dropna(inplace=True)
+    Args:
+        file_name: The name of the input CSV file.
 
-    glucose['Glucose Value (mg/dL)'] = glucose['Glucose Value (mg/dL)'].astype(float)
-    glucose = glucose.rename(columns={'Timestamp (YYYY-MM-DDThh:mm:ss)': 'Timestamp'})
+    Returns:
+        A preprocessed Pandas DataFrame containing insulin data.
+
+    Raises:
+        Exception: If the input CSV file is missing or corrupted.
+    """
+    # Read the CSV file and store it in a DataFrame
+    df = read_csv_file(file_name)
+    # Preprocess the insulin data and return the resulting DataFrame
+    insulin = _preprocess_insulin_data(df)
+
+    return insulin
+
+
+def get_glucose_data(file_name: str) -> pd.DataFrame:
+    """
+    Create a dataframe containing glucose data from a given CSV file, preprocess it by removing nulls and renaming columns.
+
+    Args:
+        file_name: The name of the input CSV file.
+
+    Returns:
+        A preprocessed Pandas DataFrame containing glucose data.
+
+    Raises:
+        Exception: If the input CSV file is missing or corrupted.
+    """
+    # Read the CSV file and store it in a DataFrame
+    df = read_csv_file(file_name)
+    # Preprocess the glucose data and return the resulting DataFrame
+    glucose = _preprocess_glucose_data(df)
 
     return glucose
 
 
-def get_insulin(file_name: str) -> pd.DataFrame:
+def _preprocess_glucose_data(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Method that creates a dataframe with insulin instances from the cvs, further processes by removing nulls and renames column titles.
-    Assumes the file name is at the same path as this file.
-    It needs the Pandas import to work and also the Path library.
-    Raises: if the input csv file is missing or corrupted
+    Preprocess the DataFrame containing glucose data by removing nulls and renaming columns.
+
+    Args:
+        df: A Pandas DataFrame containing the glucose data.
+
+    Returns:
+        A preprocessed Pandas DataFrame containing glucose data.
     """
-    file_path = CURRENT_PATH_CWD / file_name
+    # Select the relevant columns from the input DataFrame
+    glucose = df[['Timestamp (YYYY-MM-DDThh:mm:ss)', 'Glucose Value (mg/dL)']]
 
-    if not file_path.exists():
-        raise Exception(f'The expected input file name does not exist at path: {file_path}')
+    # Convert the timestamp column to datetime format
+    glucose['Timestamp (YYYY-MM-DDThh:mm:ss)'] = pd.to_datetime(glucose['Timestamp (YYYY-MM-DDThh:mm:ss)'])
 
-    df = pd.read_csv(filepath_or_buffer=file_path, index_col=0)
+    # Convert the glucose value column to numeric, setting any errors to NaN
+    glucose['Glucose Value (mg/dL)'] = pd.to_numeric(glucose['Glucose Value (mg/dL)'], errors='coerce')
 
-    insulin = df[['Timestamp (YYYY-MM-DDThh:mm:ss)', 'Event Subtype' ,'Insulin Value (u)']]
+    # Remove rows with missing values
+    glucose.dropna(inplace=True)
 
+    # Convert the glucose value column to float data type
+    glucose['Glucose Value (mg/dL)'] = glucose['Glucose Value (mg/dL)'].astype(float)
+
+    # Rename the timestamp column for better readability
+    glucose.rename(columns={'Timestamp (YYYY-MM-DDThh:mm:ss)': 'Timestamp'}, inplace=True)
+
+    return glucose
+
+
+def _preprocess_insulin_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Preprocess the DataFrame containing insulin data by removing nulls and renaming columns.
+
+    Args:
+        df: A Pandas DataFrame containing the insulin data.
+
+    Returns:
+        A preprocessed Pandas DataFrame containing insulin data.
+    """
+    # Select the relevant columns from the input DataFrame
+    insulin = df[['Timestamp (YYYY-MM-DDThh:mm:ss)', 'Event Subtype', 'Insulin Value (u)']]
+
+    # Convert the timestamp column to datetime format
     insulin['Timestamp (YYYY-MM-DDThh:mm:ss)'] = pd.to_datetime(insulin['Timestamp (YYYY-MM-DDThh:mm:ss)'])
 
+    # Convert the insulin value column to numeric, setting any errors to NaN
     insulin['Insulin Value (u)'] = pd.to_numeric(insulin['Insulin Value (u)'], errors='coerce')
 
+    # Remove rows with missing values
     insulin.dropna(inplace=True)
-    # condition_postion=insulin[(insulin["Event Subtype"]!="Long-Acting") | (insulin["Event Subtype"]!="Fast-Acting")].index
-    # insulin.drop(condition_postion, inplace=True)
 
+    # Convert the insulin value column to float data type
     insulin['Insulin Value (u)'] = insulin['Insulin Value (u)'].astype(float)
-    insulin = insulin.rename(columns={'Timestamp (YYYY-MM-DDThh:mm:ss)': 'Timestamp'})
-    insulin = insulin.rename(columns={'Event Subtype': 'Type'})
-    insulin = insulin.rename(columns={'Insulin Value (u)': 'Value'})
+
+    # Rename the columns for better readability
+    insulin.rename(
+        columns={'Timestamp (YYYY-MM-DDThh:mm:ss)': 'Timestamp', 'Event Subtype': 'Type', 'Insulin Value (u)': 'Value'},
+        inplace=True)
 
     return insulin
