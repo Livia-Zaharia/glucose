25c1515a0aa552d235643b95e32ac238ea406141
Stefan S <73125240+ownrepo1s@users.noreply.github.com>
2023-04-27 12:05:43 +0300

    The updated code has the following improvements: (#19)
    1. The default value of glucose and insulin in the __init__ method has been changed from an empty list ([]) to None. This is to avoid potential issues with mutable default arguments. Inside the constructor, glucose and insulin are set to an empty DataFrame if their value is None.
    2. Added inline comments to explain each line of code in the trend_setting method.
    3. PEP8 on the whole file

--- data_division.py
+++ data_division.py
@@ -4,9 +4,11 @@ Data division method- it takes the raw values from the Dataframe and splits it i
 
 import copy
 import typing as t
-import pandas as pd
-from ripple import Ripple
 from datetime import datetime
+
+import pandas as pd
+
+from ripple import Ripple
 
 
 class Divide:
@@ -14,34 +16,42 @@ class Divide:
     Class that contains methods for division
     """
 
-    def __init__(self,glucose:pd.DataFrame=[], insulin:pd.DataFrame=[]):
-        self.glucose=glucose
-        self.insulin=insulin
+    def __init__(self, glucose: pd.DataFrame = None, insulin: pd.DataFrame = None):
+        self.glucose = glucose if glucose is not None else pd.DataFrame()
+        self.insulin = insulin if insulin is not None else pd.DataFrame()
 
-    def trend_setting(self)->t.List[int]:
+    def trend_setting(self) -> t.List[int]:
         """
         Method that sets the trends- basically takes the values extracted using pandas in csv_insert()
         and then compares with the one before and after.
-        By storing the difference between those two we have negative and positive values which is the trend.
-        
+        By storing the difference between those two, we have negative and positive values, which is the trend.
         """
 
-        temp_trend_list= []
+        # Initialize an empty list to store the trends
+        temp_trend_list = []
 
-        for k in range(0,len(self.glucose)-1):
-            a_n=int(self.glucose.iloc[k,1].astype(int))
-            a_n_1=int(self.glucose.iloc[k+1,1].astype(int))
-            
-            
-            if a_n==a_n_1:
+        # Iterate through each glucose value from the beginning to the second to last value
+        for k in range(0, len(self.glucose) - 1):
+            # Get the current glucose value (a_n) and convert it to an integer
+            a_n = int(self.glucose.iloc[k, 1].astype(int))
+            # Get the next glucose value (a_n_1) and convert it to an integer
+            a_n_1 = int(self.glucose.iloc[k + 1, 1].astype(int))
+
+            # Check if the current glucose value is equal to the next glucose value
+            if a_n == a_n_1:
+                # If they are equal, append 0 to the temp_trend_list
                 temp_trend_list.append(0.0)
             else:
-                temp_trend_list.append(a_n_1-a_n)
-            
+                # If they are not equal, append the difference between the next and current glucose value
+                temp_trend_list.append(a_n_1 - a_n)
+
+        # Append 0 to the temp_trend_list for the last glucose value
         temp_trend_list.append(0.0)
+
+        # Return the list of trends
         return temp_trend_list
 
-    def parting(self,trend_list:t.List[int],threshold:int)->t.List[int]:
+    def parting(self, trend_list: t.List[int], threshold: int) -> t.List[int]:
 
         """
         Method that divides the values based on trend that changes sign- 
@@ -49,79 +59,74 @@ class Divide:
 
         """
 
-        trend_list_count=[]
+        trend_list_count = []
 
-        count=0
-        count_positive=0
-        count_negative=0
-        k=0
+        count = 0
+        count_positive = 0
+        count_negative = 0
+        k = 0
 
-        switch=0
+        switch = 0
 
-        positive_trend=0
-        negative_trend=0
+        positive_trend = 0
+        negative_trend = 0
 
-        positive_trend_prev=0
-        negative_trend_prev=0
+        positive_trend_prev = 0
+        negative_trend_prev = 0
 
-        
-        while k<len(self.glucose):
-        
-            a_n=trend_list[k]
-        
-            if a_n>=0 and k<len(self.glucose):
-                
-                while a_n>=0 and k<len(self.glucose)-1:
-                    count_positive+=1
-                    k+=1
-                    
-                    positive_trend+=a_n
-                    a_n=trend_list[k]
-                    count+=1
-                
-                
-                positive_trend=positive_trend/count_positive
-                switch+=1
+        while k < len(self.glucose):
 
-            elif a_n<0 and k<len(self.glucose):
-                
-                while a_n<0 and k<len(self.glucose)-1:
-                    count_negative+=1
-                    k+=1
-                    
-                    negative_trend+=a_n
-                    a_n=trend_list[k]
-                    count+=1
-                
-                
-                negative_trend=negative_trend/count_negative
-                negative_trend=negative_trend*(-1)
-                switch+=1
+            a_n = trend_list[k]
 
-            if switch>=2 and count >50:
-                
-                count_positive=0
-                count_negative=0
+            if a_n >= 0 and k < len(self.glucose):
 
-                if (positive_trend>=threshold and
-                     negative_trend>=threshold) or (positive_trend_prev>=threshold and
-                     negative_trend>=threshold) or (positive_trend>=threshold and
-                     negative_trend_prev>=threshold):
-                    
+                while a_n >= 0 and k < len(self.glucose) - 1:
+                    count_positive += 1
+                    k += 1
+
+                    positive_trend += a_n
+                    a_n = trend_list[k]
+                    count += 1
+
+                positive_trend = positive_trend / count_positive
+                switch += 1
+
+            elif a_n < 0 and k < len(self.glucose):
+
+                while a_n < 0 and k < len(self.glucose) - 1:
+                    count_negative += 1
+                    k += 1
+
+                    negative_trend += a_n
+                    a_n = trend_list[k]
+                    count += 1
+
+                negative_trend = negative_trend / count_negative
+                negative_trend = negative_trend * (-1)
+                switch += 1
+
+            if switch >= 2 and count > 50:
+
+                count_positive = 0
+                count_negative = 0
+
+                if (positive_trend >= threshold and
+                    negative_trend >= threshold) or (positive_trend_prev >= threshold and
+                                                     negative_trend >= threshold) or (positive_trend >= threshold and
+                                                                                      negative_trend_prev >= threshold):
                     trend_list_count.append(count)
-                    count=0
-                    switch=0
-                
-                positive_trend_prev=positive_trend
-                negative_trend_prev=negative_trend
+                    count = 0
+                    switch = 0
 
-                positive_trend=0
-                negative_trend=0
-            
-                
-            if k==len(self.glucose) - 1:
-                    k+=1
-        
+                positive_trend_prev = positive_trend
+                negative_trend_prev = negative_trend
+
+                positive_trend = 0
+                negative_trend = 0
+
+            if k == len(self.glucose) - 1:
+                k += 1
+
         return trend_list_count
 
     def generate_ripples(self, trend_list: t.List[int], trend_list_count: t.List[int]) -> t.List[Ripple]:
@@ -148,17 +153,17 @@ class Divide:
 
         return ripple_list
 
-    def divide_by_iterable(self,data:Ripple)->t.Tuple[dict,dict]:
+    def divide_by_iterable(self, data: Ripple) -> t.Tuple[dict, dict]:
         """
         Method that splits any given ripple into dictionaries of iterable and respectively non iterable elements
         """
 
-        to_be_processed=copy.deepcopy(dict(vars(data)))
-        data_iter=copy.deepcopy(to_be_processed)
-        data_noniter=copy.deepcopy(to_be_processed)
+        to_be_processed = copy.deepcopy(dict(vars(data)))
+        data_iter = copy.deepcopy(to_be_processed)
+        data_noniter = copy.deepcopy(to_be_processed)
 
         for item in list(to_be_processed.keys()):
-                    
+
             try:
                 iter(to_be_processed[item])
             except TypeError:
@@ -166,19 +171,19 @@ class Divide:
             else:
                 data_noniter.pop(item)
 
-        return (data_iter,data_noniter)
-    
-    def split_insulin_by_ripple(self, ripple_list:t.List[Ripple])->t.List[t.List[t.Tuple[datetime,str,float]]]:
-         """
-         Method that splits the insulin dataframe stored into a list of n lists (the lenght of the ripple list recieved) with tuples 
-         structured as(timestamp-type of insulin-dosage)
-         """
-         insulin_storage=[]
-         i=0
-         for elem in ripple_list:
-             insulin_list=[]
-             while self.insulin.iloc[i,0]<=elem.end_t:
-                 insulin_list.append((self.insulin.iloc[i,0],self.insulin.iloc[i,1],self.insulin.iloc[i,2]))
-                 i+=1
-             insulin_storage.append(insulin_list)
-         return insulin_storage
+        return (data_iter, data_noniter)
+
+    def split_insulin_by_ripple(self, ripple_list: t.List[Ripple]) -> t.List[t.List[t.Tuple[datetime, str, float]]]:
+        """
+        Method that splits the insulin dataframe stored into a list of n lists (the lenght of the ripple list recieved) with tuples
+        structured as(timestamp-type of insulin-dosage)
+        """
+        insulin_storage = []
+        i = 0
+        for elem in ripple_list:
+            insulin_list = []
+            while self.insulin.iloc[i, 0] <= elem.end_t:
+                insulin_list.append((self.insulin.iloc[i, 0], self.insulin.iloc[i, 1], self.insulin.iloc[i, 2]))
+                i += 1
+            insulin_storage.append(insulin_list)
+        return insulin_storage
